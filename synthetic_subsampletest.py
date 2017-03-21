9f3d1bf54fb1b82106acf77a8f6910c2d736bb04
Naheed_dbrl <naheed_anjum@u.nus.edu>
2017-03-21 16:36:50 +0800

    Added synthetic graph staff

---
+++ synthetic_subsampletest.py
@@ -0,0 +1,98 @@
+__author__ = 'workshop'
+
+
+import os, sys
+import csv
+from operator import itemgetter
+
+
+def aggregateresults(sourcepath, outputfilename):
+    assert os.path.isdir(sourcepath) == True
+    flagmeaning = {0: 'Star', 1: 'Complete', 2: 'Wheel', 3: 'Circle'}
+    algoht = {}
+    for file in os.listdir(sourcepath):
+        print file
+        with open(sourcepath + "/" + file, 'r') as f:
+            all = f.readlines()
+            flag = -1
+            numhg = -1
+            for i, val in enumerate(range(len(all))):
+                line = all[i]
+
+                if numhg == -1:
+                    if line.startswith("# hyperedges"):
+                        numhg = int(line.split()[-1])
+
+                if line.startswith("Star"):
+                    flag = 0
+                    algoht['Star'] = algoht.get('Star', [])
+
+                if line.startswith("Complete"):
+                    flag = 1
+                    algoht['Complete'] = algoht.get('Complete', [])
+
+                if line.startswith("Wheel"):
+                    flag = 2
+                    algoht['Wheel'] = algoht.get('Wheel', [])
+
+                if line.startswith("Circle"):
+                    flag = 3
+                    algoht['Circle'] = algoht.get('Circle', [])
+
+                if flag != -1:
+                    # numint = all[i+1].split()[slice(2,5,2)]
+                    # areprop = all[i+2].split()[slice(2,5,2)]
+                    # concav = all[i+3].split()[slice(2,5,2)]
+
+
+                    numint = [float(x) for x in all[i + 1].split()[slice(2, 5, 2)]]
+                    areprop = [float(x) for x in all[i + 2].split()[slice(2, 5, 2)]]
+                    concav = [float(x) for x in all[i + 3].split()[slice(2, 5, 2)]]
+
+                    try:
+                        uniformity = ' '.join(all[i + 4].split()[1:])
+                    except:
+                        print all[i], all[i + 1], all[i + 2], all[i + 3]
+                        sys.exit(1)
+                    # print flagmeaning[flag], algoht[flagmeaning[flag]],algoht['Star']
+                    if algoht[flagmeaning[flag]] != []:
+                        # print algoht
+                        algoht[flagmeaning[flag]].append([numhg] + numint + areprop + concav)
+                        algoht[flagmeaning[flag]][-1].append(uniformity)
+                    else:
+                        algoht[flagmeaning[flag]] = [[numhg] + numint + areprop + concav]
+                        algoht[flagmeaning[flag]][-1].append(uniformity)
+                    flag = -1
+    #
+    # for x in algoht['Star']:
+    #     print x , len(x)
+
+    for k, v in flagmeaning.items():
+        algoht[v] = sorted(algoht[v], key=itemgetter(0), reverse=True)
+
+    for k, v in flagmeaning.items():
+        # csvf = open(sourcepath + "/" + v + "_" + outputfilename, 'wt')
+        csvf = open(sourcepath + "\\" + v + "_" + outputfilename, 'wt')
+
+        try:
+            writer = csv.writer(csvf)
+            writer.writerow(('NumHyperedges', 'IntersectionMean', 'IntersectionSD', 'AreaPropMean', 'AreaPropSD',
+                             'ConcavityMean', 'ConcavitySD', 'Uniformity'))
+            for line in algoht[v]:
+                writer.writerow(line)
+        finally:
+            csvf.close()
+
+
+def main():
+    # rootpath = "/Users/naheed/Google Drive/fuchterman_reingold_everything/datasets"
+    rootpath = "C:\\Users\\workshop\\Google Drive\\fuchterman_reingold_everything\\datasets\\synthetic"
+    suffixpath = "subsampletest"
+
+    outputfilename = "res_agg.csv"
+    # aggregateresults(rootpath + "/" + suffixpath, outputfilename)
+    aggregateresults(rootpath + "\\" + suffixpath, outputfilename)
+
+if __name__ == "__main__":
+    main()
+
